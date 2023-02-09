b1cf6ef14aaa3b2fe7f8f49d4ce8eae6d6d0d8e6
Crowdbotics <engineering@crowdbotics.com>
2023-02-09 02:08:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/summer_fog_dev_62250/MainActivity.java
@@ -0,0 +1,15 @@
+package com.summer_fog_dev_62250;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "summer_fog_dev_62250";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-fog-dev-62250",
+  url: "https://summer-fog-dev-62250.botics.co",
+  api: "https://summer-fog-dev-62250.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
